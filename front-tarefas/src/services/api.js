e3891e9bef2010605e0375a4641e15fd4aa3d73c
Jean Pedro <jjeanpedro20@gmail.com>
2026-04-20 17:19:10 -0300

    front pronto para deploy

--- front-tarefas/src/services/api.js
+++ front-tarefas/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_URL || "https://krona-backend-77345390046.southamerica-east1.run.app",
 });
 
 export default API;
