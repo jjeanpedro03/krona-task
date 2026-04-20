076918a7ced81c2cf5bfa6f24ef077db7e9bfc50
Jean Pedro <jjeanpedro20@gmail.com>
2026-04-20 15:47:48 -0300

    fix: conexão com banco Railway

--- back-tarefas/db.js
+++ back-tarefas/db.js
@@ -1,24 +1,12 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error("❌ ERRO AO CONECTAR NO BANCO:", err.message);
-  } else {
-    console.log("✅ BANCO CONECTADO COM SUCESSO!");
-    conn.release();
-  }
-});
-
-module.exports = pool.promise();
+module.exports = db;
